a2c589d2e7a686a10267ba28bb5e3d81551fa4e4
Alexandre Juan <juandev-pro@protonmail.com>
2022-04-20 10:37:13 +0200

    feat(): add smart generation data (append instead of rewrite) + add start of help + refactor code

--- get_data.py
+++ get_data.py
@@ -1,3 +1,6 @@
+import os
+import sys
+import timeit
 from mysqlx import Auth
 from numpy import NaN
 from umls_api.mysql_connection import db, UMLS_API_KEY
@@ -6,82 +9,136 @@ from umls_api.semantic_network_queries import SemanticNetworkQueries
 from umls_api.authentication_umls_api import Authentication
 import pandas as pd
 
-meta = MetathesaurusQueries(db)
-sty = SemanticNetworkQueries(db)
-auth = Authentication(UMLS_API_KEY)
 
-def print_description(label, sab, cui, sty_label, tui):
+def print_description(concept_label, concept_sab, concept_cui,
+                      concept_lui, concept_sty_label, concept_tui):
     print("\n")
-    print("label", label)
-    print("sab", sab)
-    print("cui", cui)
-    print("sty_label", sty_label)
-    print("tui", tui)
+    print("label", concept_label)
+    print("sab", concept_sab)
+    print("cui", concept_cui)
+    print("lui", concept_lui)
+    print("sty_label", concept_sty_label)
+    print("tui", concept_tui)
     print("\n")
 
-gui_indexes = {}
-all_gui = sty.get_all_gui()
-for index, item in enumerate(all_gui):
-    gui_indexes[item[1]] = item[0]
 
-res = meta.get_all_mrcon_with_sty(nb_data=500)
-count = 0
-X = []
-Y = []
-for (label, sab, cui, sty_label, tui) in res:
-    print_description(label, sab, cui, sty_label, tui)
+def save_index_db(count, db_index_path="db_index"):
+    # Write db_index value in db_index file
+    text_file = open(db_index_path, "w+")
+    text_file.write(str(count))
+    text_file.close()
 
-    source = sab
-    code = meta.get_code_from_cui_and_source(cui, source)
-    has_nb_parents_and_children = True
-    if code == None:
-        # Set firstly -1 to the nb_all_parents and nb_all_children
-        # Then, we'll set the value to the mean of the nb_all_parents and nb_all_children
-        nb_parents = -1
-        nb_children = -1
-        has_nb_parents_and_children = False
+
+def save_to_csv(X, Y, db_index):
+    data = pd.DataFrame(X, columns=['Label', 'Source', 'CUI', 'Nb_Parents', 'Nb_Children',
+                                    'Nb_Parents_Children_Known', 'Definition', 'Has_Definition'])
+    data = pd.concat([data, pd.DataFrame(Y, columns=['TUI', 'GUI'])], axis=1)
+    # Save X and Y in the same csv file
+    if (db_index > 0):
+        data.to_csv('data.csv', mode="a", index=False, header=False)
     else:
-        nb_parents = meta.get_nb_all_parents_from_cui_from_umls_api(auth, source, code)
-        nb_children = meta.get_nb_all_children_from_cui_from_umls_api(auth, source, code)
+        data.to_csv('data.csv', index=False)
 
-    definition = meta.get_definition_from_cui_and_source(cui, source)
-    has_definition = True
-    if not definition:
-        has_definition = False
 
-    row = [
-        label,                                      # 0 Label -> is deleted in the end because it is not useful right now
-        sab,                                        # 0 SAB -> is deleted in the end because it is not useful
-        cui,                                        # 0 CUI -> is deleted in the end because it is not useful
-        nb_parents if nb_parents != -1 else NaN,    # 1 Number of parents
-        nb_parents if nb_children != -1 else NaN,   # 2 Number of children
-        1 if has_nb_parents_and_children else 0,    # 3 Has nb_parents and nb_children
-        definition,                                 # 4 Definition
-        1 if has_definition else 0,                 # 5 Has definition
-    ]
-    X.append(row)
-    Y.append([tui, gui_indexes[tui]])
-    count += 1
+def main():
+    db_index = None
+    db_index_path = "db_index"
 
-print("Number of rows:", count)
-data = pd.DataFrame(X, columns=['Label', 'Source', 'CUI', 'Nb_Parents', 'Nb_Children', 'Has_Nb_Parents_And_Children', 'Definition', 'Has_Definition'])
-data = pd.concat([data, pd.DataFrame(Y, columns=['TUI', 'GUI'])], axis=1)
+    if len(sys.argv) > 1 and sys.argv[1] == "--reset":
+        # Reset db_index file
+        save_index_db(0, db_index_path)
+        # remove data.csv file
+        if os.path.exists('data.csv'):
+            os.remove('data.csv')
+        exit(0)
 
-################ Nb_Parents and Nb_Children ###############
+    if len(sys.argv) > 1 and sys.argv[1] == "--help":
+        print("Usage:")
+        print("\tpython3 get_data.py [--help] [--index]")
+        exit(0)
+    elif len(sys.argv) > 1 and sys.argv[1] == "--index":
+        if os.path.isfile(db_index_path):
+            text_file = open(db_index_path, "r")
+            db_index = int(text_file.read())
+            text_file.close()
+            print("Index Database: ", db_index)
+        else:
+            print("No db_index file found")
+            exit(84)
 
-# # Prepare the nb_parents and nb_children to be used in the model # TODO set in the notebook
-# mean_all_parents = data['Nb_Parents'].mean()
-# mean_all_children = data['Nb_Children'].mean()
-# print("Mean of parents", mean_all_parents)
-# print("Mean of children", mean_all_children)
-# data.loc[data['Nb_Parents'].isnull(), 'Nb_Parents'] = mean_all_parents
-# data.loc[data['Nb_Children'].isnull(), 'Nb_Children'] = mean_all_children
+    start = timeit.default_timer()
 
-# # Normalize the data # TODO set in the notebook
-# data['Nb_Parents'] = (data['Nb_Parents'] - data['Nb_Parents'].min()) / (data['Nb_Parents'].max() - data['Nb_Parents'].min())
-# data['Nb_Children'] = (data['Nb_Children'] - data['Nb_Children'].min()) / (data['Nb_Children'].max() - data['Nb_Children'].min())
+    meta = MetathesaurusQueries(db)
+    sty = SemanticNetworkQueries(db)
+    auth = Authentication(UMLS_API_KEY)
 
-###########################################################
+    gui_indexes = {}
+    all_gui = sty.get_all_gui()
+    for index, item in enumerate(all_gui):
+        gui_indexes[item[1]] = item[0]
 
-# Save X and Y in the same csv file
-data.to_csv('data.csv', index=False)
+    res = meta.get_all_mrcon_with_sty(nb_data=500, offset=db_index)
+    count = 0
+    X = []
+    Y = []
+    try:
+        for (label, sab, cui, lui, sty_label, tui) in res:
+            print_description(label, sab, cui, lui, sty_label, tui)
+
+            source = sab
+            aui = meta.get_aui_from_cui_and_source_and_lui(cui, source, lui)
+            if aui:
+                nb_parents_children_known = 1
+                print("Getting number of ancestors...")
+                nb_parents = meta.get_nb_all_parents_from_aui_and_umls_api(
+                    auth, aui)
+                print("Getting number of descendants...")
+                nb_children = meta.get_nb_all_children_from_aui_and_umls_api(
+                    auth, aui)
+                if nb_children == -1 and nb_parents == -1:
+                    nb_parents_children_known = 0
+                else:
+                    nb_parents = 0 if nb_parents == -1 else nb_parents
+                    nb_children = 0 if nb_children == -1 else nb_children
+            else:
+                nb_parents_children_known = 0
+
+            print("Getting definition...")
+            definition = meta.get_definition_from_cui_and_source(cui, source)
+            print("Done.")
+            has_definition = True
+            if not definition:
+                has_definition = False
+
+            row = [
+                label,
+                sab,
+                cui,
+                nb_parents if nb_parents != -1 else NaN,
+                nb_children if nb_children != -1 else NaN,
+                1 if nb_parents_children_known else 0,
+                definition,
+                1 if has_definition else 0,
+            ]
+            X.append(row)
+            Y.append([tui, gui_indexes[tui]])
+            count += 1
+            print("\n")
+    except KeyboardInterrupt:
+        print("Quitting...")
+        save_to_csv(X, Y, db_index)
+        save_index_db(db_index + count)
+        exit(0)
+
+    print("Number of rows:", count)
+
+    save_to_csv(X, Y, db_index)
+    save_index_db(db_index + count)
+
+    stop = timeit.default_timer()
+
+    print('Time: ', stop - start)
+
+
+if __name__ == '__main__':
+    main()
