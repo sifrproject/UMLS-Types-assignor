927c0f0b0d7fc8ca64223111ffc94c535a471786
Alexandre Juan <juandev-pro@protonmail.com>
2022-07-25 10:24:42 +0200

    feat: add error handling end of tree

--- umls_api/bioportal_api.py
+++ umls_api/bioportal_api.py
@@ -71,31 +71,35 @@ class BioPortalAPI:
         return results
 
     def get_features_from_link(self, link, parents_code_id):
-        results = self.request_url(link)
-        if results is None:
-            print("get_features_from_link", "No results found.")
+        try:
+            results = self.request_url(link)
+            if results is None:
+                print("get_features_from_link", "No results found.")
+                return None
+            try:
+                labels = " ".join([i for i in results['synonym']])
+            except:
+                labels = None
+            try:
+                definition = " ".join([i for i in results['definition']])
+            except:
+                definition = None
+            json = {
+                "pref_label": results['prefLabel'],
+                "labels": results['prefLabel'] + ' ' + labels,
+                "definition": definition,
+                "has_definition": True if definition is not None and len(definition) > 0 else False,
+                "source": self.source if self.source is not None else '',
+                "parents_type": None,
+                "semantic_type": results['semanticType'],
+                "code_id": results['@id'],
+                "parents_code_id": parents_code_id,
+                "children": results['links']['children']
+            }
+            return json
+        except Exception as e:
+            print("get_features_from_link", str(e))
             return None
-        try:
-            labels = " ".join([i for i in results['synonym']])
-        except:
-            labels = None
-        try:
-            definition = " ".join([i for i in results['definition']])
-        except:
-            definition = None
-        json = {
-            "pref_label": results['prefLabel'],
-            "labels": results['prefLabel'] + ' ' + labels,
-            "definition": definition,
-            "has_definition": True if definition is not None and len(definition) > 0 else False,
-            "source": self.source if self.source is not None else '',
-            "parents_type": None,
-            "semantic_type": results['semanticType'],
-            "code_id": results['@id'],
-            "parents_code_id": parents_code_id,
-            "children": results['links']['children']
-        }
-        return json
 
     def get_children_links(self, children_collection_link):
         results = self.request_url(children_collection_link)
